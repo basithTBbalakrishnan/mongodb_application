425df2438a12ba948f42c22a8df5014b6c9635bf
Namitha Chandrasekhar <namitha.chandran@vakilsearch.com>
2022-04-18 16:26:58 +0530

    The initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User 
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :firstname, type: String
+  field :lastname, type: String
+  field :email, type: String
+end
---
+++ db/seed.rb
@@ -0,0 +1,5 @@
+#sample user seed data
+User.create(firstname:"albert",lastname: "einstein",email: "ae@relativity.com")
+User.create(firstname:"marie",lastname: "curie",email: "mc@radiation.com")
+User.create(firstname:"issac",lastname: "newton",email: "in@gravity.com")
+User.create(firstname:"gallileo",lastname: "galilei",email: "gg@astronomy.com")
